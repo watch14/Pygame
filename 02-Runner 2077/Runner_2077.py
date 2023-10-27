ddc5badd0a0f2dde841a84fb2b3136d4d2e5d746
watch14 <watchman1414@gmail.com>
2023-10-27 15:12:39 +0100

    runner 2077done

---
+++ 02-Runner 2077/Runner_2077.py
@@ -0,0 +1,279 @@
+from typing import Any
+import pygame
+from sys import exit
+
+from random import randint, choice
+
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_walk_1= pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+        player_walk_1 = pygame.transform.scale_by(player_walk_1, 0.5)
+        player_walk_2= pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+        player_walk_2 = pygame.transform.scale_by(player_walk_2, 0.5)
+
+        self.player_walk = [player_walk_1, player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+        self.player_jump = pygame.transform.scale_by(self.player_jump, 0.5)
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom = (130, 299))
+        self.gravity = 0
+
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.06)
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 299 :
+            self.gravity = -11
+            self.jump_sound.play()
+
+    def apply_gravity(self):
+        self.gravity += 0.3
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 299 : self.rect.bottom = 299
+
+    def animation_state(self):
+        if self.rect.bottom < 299:
+            self.image = self.player_jump
+        else :
+            self.player_index += 0.09
+            if self.player_index >= len(self.player_walk): self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__ (self,type):
+        super().__init__()
+
+        if type == 'fly':
+            fly_f1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
+            fly_f1 = pygame.transform.scale_by(fly_f1, 0.7)
+            fly_f2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
+            fly_f2 = pygame.transform.scale_by(fly_f2, 0.7)
+
+
+            self.frames = [fly_f1,fly_f2]
+            y_pos = randint(120, 180)
+        else :
+            snail_f1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+            snail_f1 = pygame.transform.scale_by(snail_f1, 0.7)
+            snail_f2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+            snail_f2 = pygame.transform.scale_by(snail_f2, 0.7)
+
+
+            self.frames = [snail_f1, snail_f2]
+            y_pos = 299
+        
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(900, 1200), y_pos))
+    
+    def animation_state(self):
+        self.animation_index += 0.07
+        if self.animation_index >= len(self.frames): self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+
+    def destroy(self):
+        if self.rect.x <= -100: self.kill()
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= 5
+        self.destroy()
+
+
+def display_score():
+
+    curent_time = int (pygame.time.get_ticks() / 1000) - start_time
+    score_surf = font_test.render(f'{curent_time}', False, 'black')
+    score_rect = score_surf.get_rect(center = (400, 30))
+
+    screen.blit(score_surf, score_rect)
+    return curent_time
+
+
+def collision_sprite():
+    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
+        obstacle_group.empty()#reset position if game over
+        return False
+    else : return True
+
+c = "font/Pixeltype.ttf"
+
+
+pygame.init()
+
+## screen display
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner 2077')     ## Game name
+Clock = pygame.time.Clock()     ## Max frame rate
+font_test = pygame.font.Font('font/Pixeltype.ttf', 35)    # font type and size
+splash_img = pygame.image.load('graphics/bg.png').convert()
+
+#music
+bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music.set_volume(0.03)
+bg_music.play(loops = -1)
+
+start_time = None
+score = 0
+
+## load images
+sky_img = pygame.image.load('graphics/Sky3.png').convert()
+sky_rect = sky_img.get_rect(topleft = (0,0 ))
+sky_rect2 = sky_img.get_rect(topleft = (800,0 ))
+
+ground_img = pygame.image.load('graphics/ground2.png').convert()
+ground_rect = ground_img.get_rect(topleft = (0,299 ))
+ground_rect2 = ground_img.get_rect(topleft = (800,299 ))
+
+## Groups 
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+obstacle_group = pygame.sprite.Group()
+
+
+
+
+##############################################################################
+    # start and end screen
+runner_surf = font_test.render('Runner 2077', False, 'white')
+runner_surf = pygame.transform.scale_by(runner_surf, 3)
+runner_rect = runner_surf.get_rect(center = (400, 70))
+
+    #intro player image
+player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand= pygame.transform.scale_by(player_stand, 1)
+stand_rect = player_stand.get_rect(center = (400, 200))
+
+
+press_surf = font_test.render('Press SPACE to Restart', False, 'white')
+press_rect = press_surf.get_rect(center = (400, 330))
+##############################################################################
+
+
+obstacle_rect_list = []
+
+
+
+#timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1100)
+
+snail_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(snail_animation_timer, 700)
+
+fly_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(fly_animation_timer, 300)
+
+
+
+#game variable
+game_active = False
+new_game = True
+
+## main 
+while True:
+    
+    for event in pygame.event.get():
+        ## Quit ##
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        if game_active:
+      
+        
+            if event.type == obstacle_timer:
+            #randomizer spawn snail if 0. fly if 2 
+                obstacle_group.add(Obstacle(choice(['fly','snail','snail'])))        
+
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                start_time = int (pygame.time.get_ticks() / 1000) 
+
+    if game_active:
+        
+        ## all elaments
+
+        screen.blit(sky_img,sky_rect)
+        screen.blit(sky_img,sky_rect2)
+
+        screen.blit(ground_img,ground_rect)
+        screen.blit(ground_img,ground_rect2)
+
+        ## background movement
+        sky_rect.left -= 2
+        sky_rect2.left -= 2
+
+        if sky_rect.right <= 0:
+            sky_rect.left = sky_rect2.right
+        if sky_rect2.right <= 0:
+            sky_rect2.left = sky_rect.right
+
+
+        ## ground movement
+        ground_rect.left -= 2
+        ground_rect2.left -= 2
+
+        if ground_rect.right <= 0:
+            ground_rect.left = ground_rect2.right
+        if ground_rect2.right <= 0:
+            ground_rect2.left = ground_rect.right
+    
+
+        score = display_score()
+
+      
+        # player
+        player.draw(screen)
+        player.update()
+
+        #obstacle
+        obstacle_group.draw(screen)
+        obstacle_group.update()
+
+        #collisions
+        game_active = collision_sprite()       
+
+
+        new_game = False
+
+      
+    
+    else :
+        screen.fill((94, 129, 162))
+        screen.blit(splash_img,(0, 0))
+        obstacle_rect_list.clear()   # clear enemies when game over
+        if new_game:
+            # Title Runner 2077
+            screen.blit(runner_surf, runner_rect)
+        else :
+            ## score
+            over_surf = font_test.render(f'GAME OVER : {score}', False, 'white') 
+            over_surf = pygame.transform.scale_by(over_surf, 1.5)
+            over_rect = over_surf.get_rect(center = (400, 70))
+            screen.blit(over_surf, over_rect)
+        
+        #Press space
+        screen.blit(press_surf, press_rect)
+        screen.blit(player_stand, stand_rect)
+
+
+
+
+    pygame.display.update()   ## updare every thing
+    Clock.tick(120)      ## Max fps
---
+++ 02-Runner 2077/Runner_2077.spec
@@ -0,0 +1,38 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['Runner_2077.py'],
+    pathex=[],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name='Runner_2077',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    icon=['pic.ico'],
+)
